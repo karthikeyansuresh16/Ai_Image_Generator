8fbfe1700fd0a5147553ed67de0d10bbac9cbb0d
karthikeyansuresh16 <karthikeyansuresh93@gmail.com>
2025-08-29 14:06:18 +0530

    css has been modified

--- src/Component/ImageGenerator/ImageGenerator.jsx
+++ src/Component/ImageGenerator/ImageGenerator.jsx
@@ -5,9 +5,14 @@ import default_image from'../Assets/Ai.jpg'
 const ImageGenerator = () => {
   return (
     <div className="image-generator">
-      <div className="Head">Ai image<span>Generator</span></div>
+      <div className="Head">Ai image<span> Generator</span></div>
       <div className="img-loading"></div>
-      <div className="image"><img src={default_image} alt="AI Generated" /></div>
+      <div className="image"><img src={default_image} alt="AI Generated" />
+    </div>
+    <div className="search-box">
+      <input type="text" className="search-input" placeholder="Describe what you want to see" />
+      <div className="generate-btn">Generate</div>
+    </div>
     </div>
   )
 }
